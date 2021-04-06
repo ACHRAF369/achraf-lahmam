166862017e1eebac76e8cdb591daf836f460f9d2
ACHRAF369 <achraflahmam2@gmail.com>
2021-04-06 11:22:46 +0100

    add project 1

---
+++ basics-js/projet-1/js/app.js
@@ -0,0 +1,8 @@
+var button = document.getElementById('submitBtn');
+button.onclick = function(){
+   var message = document.getElementById('message').value;
+   var output = document.getElementById('namestack');
+   output.innerHTML = message;
+
+    
+}
