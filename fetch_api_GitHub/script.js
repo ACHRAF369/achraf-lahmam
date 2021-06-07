549c4e4f534e43056005faff6921dccea6fbeb77
ACHRAF369 <achraflahmam2@gmail.com>
2021-06-07 15:24:54 +0100

    creat Stars Count Span and count text

--- fetch_api_GitHub/script.js
+++ fetch_api_GitHub/script.js
@@ -3,9 +3,8 @@ let getButton = document.querySelector(".btn-danger");
 let reposData = document.querySelector(".Show-Data")
 
 getButton.onclick = function(){
-
+alert("test")
     getRepos();
-
 }
 
 // Get Repos Function
@@ -21,7 +20,7 @@ function getRepos() {
         // loop on Repositories
         repositories.forEach(repo =>{
          
-        //Creat the main Div
+        //Creat the main Div Element
         let mainDiv = document.createElement("div");
          
         //creat repo Name Text
@@ -31,7 +30,46 @@ function getRepos() {
         mainDiv.appendChild(repoName);
 
         // Append the mainDiv to Container
+        //  reposData.appendChild(mainDiv);
+ 
+
+        //Creat repo URl Anchor
+        let theUrl = document.createElement('a');
+
+        //Creat Repo Url Text
+        let theUrl = document.createTextnode("visit");
+
+        // Append The Repo Url Text To Anchor Tag
+        theUrl.appendChild(theUrlText);
+
+         //Add the Hypertext Reference "href"
+         theUrl.href =`https//github.com/achraf369/${repo.name}`;
+        
+         //Set Attribut Blank
+         theUrl.setAttribute('target', '_blank');
+
+
+
+         //Append Url Anchor To Main Div
+         mainDiv.appendChild(theUrl);
+         
+         //Create Stars Count Span
+         let StarsSpan = document.createElement("span");
+
+         //Creat the strars count 
+         let starsText = document.createTextnode(`Stars ${repo.stargazers_count}`);
+
+        //Add Stars 
+
+         //Append the Main Div To Container
          reposData.appendChild(mainDiv);
+
+         //
+
+      
+         
+
+
         });
 
          });
